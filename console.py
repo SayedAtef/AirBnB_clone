f1f3388acdf06cc65e787c714c8c042caaa55c6c
Sayed Atef <sayedatef6293@gmail.com>
2023-08-13 00:52:33 +0300

    remove unwanted imports

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ AirBnB Console """
 import cmd
-from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
