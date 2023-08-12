ce23a325c1d9400dd796a090e9592d13ba6d56c2
Oluwatobi Gbadamosi <tobbybosch@gmail.com>
2023-08-12 06:05:12 -0700

    AirBnB update

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """Represents amenities
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+    """
+    __tablename__ = "py amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
