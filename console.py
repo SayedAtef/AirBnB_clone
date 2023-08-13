8ff0f55664c554363457fe00f7bc8b9b4e8c960c
Sayed Atef <sayedatef6293@gmail.com>
2023-08-13 23:39:24 +0300

    added show

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 import sys
 import os
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -47,6 +48,25 @@ class HBNBCommand(cmd.Cmd):
                     print(newInstance.id)
                 else:
                     print("** class doesn't exist **")
+
+
+    def do_show(self, arg):
+        """ Method to print string rep of instance """
+        if len(arg) == 0:
+            print('** class name missing **')
+            return
+        elif arg.split()[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(arg.split()) > 1:
+            keyInstance = arg.split()[0] + '.' + arg.split()[1]
+            if keyInstance in storage.all():
+                instance = storage.all()
+                print(instance[keyInstance])
+            else:
+                print('** no instance found **')
+        else:
+            print('** instance id missing **')
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
